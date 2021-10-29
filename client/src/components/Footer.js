20b35c86f1109303af05b89a7ebfdd2095042621
jose hernandez <josegho2013@gmail.com>
2021-10-29 18:37:15 -0300

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Styles/Footer.css";
+
+
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="footer_container">
+     
+      <div>
+        <h3>Realizado por:</h3>
+        <h4>José Hernández</h4>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
