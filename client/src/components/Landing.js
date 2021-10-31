e2eb9e3754b8da8ca7743f5c515981fb33a65c86
jose hernandez <josegho2013@gmail.com>
2021-10-31 15:15:54 -0300

    .

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import "./Styles/Landing.css";
 const Landing = () => {
     return (
-        <div>
-            
+        
+              <div className="landing">
+      <Link to="/home">
+        <button className="button">Welcome</button>
+      </Link>
         </div>
     )
 }
